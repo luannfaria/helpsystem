531d3709701bd47130ce8bacbafd11356c6f66a2
Isnack Souza <isnack_yagami@hotmail.com>
2015-04-24 13:40:55 -0300

    Login provisório feito

--- Conexao.php
+++ Conexao.php
@@ -1,7 +1,7 @@
 <?php
 $host="localhost";
 $user="root";
-$pass="clara02";
+$pass="admin";
 $banco="local";
 mysql_connect($host, $user,$pass) or die("BANCO NAO CONECTOU");
 mysql_select_db($banco)or die("nao selecionou banco");
