8c3504f8171da09223aa4be50b27b66eaaf215ef
Luan <Luan@LuanPC>
2015-04-26 15:26:31 -0300

    teste css/ login

--- Conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="localhost";
-$user="root";
-$pass="admin";
-$banco="local";
-mysql_connect($host, $user,$pass) or die("BANCO NAO CONECTOU");
-mysql_select_db($banco)or die("nao selecionou banco");
-?>
