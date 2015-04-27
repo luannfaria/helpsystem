2f59755a9158695810e57dea32ede297256193d1
Luan <Luan@LuanPC>
2015-04-26 22:31:08 -0300

    criaçao divs/paginas e remodelagem css

--- helpsystem/verificauser.php
+++ helpsystem/verificauser.php
@@ -6,15 +6,15 @@ $senha = $_POST["senha"];
         $logar = mysql_query("SELECT * FROM usuario WHERE login='$nome' AND senha='$senha'") or die("erro ao selecionar");
 
 if (strlen($senha)< 1)
-echo '<p align="center"-->Usuario ou senha não conferem<br><a href="javascript:history.back(1);">tente novamente</a><p></p>';
-
+	header("location:error.html");
+		
 elseif (mysql_num_rows($logar)>0 ){
 header("location:inicial.html");
 
 } 
 else {
 
-echo '<p align="center">usuario ou senha não conferem<br><a href="javascript:history.back(1);">tente novamente</a></p>';
+	header("location:error.html");
 
 }
 ?>
