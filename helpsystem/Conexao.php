f2fc2b6bbc45bc30d2d1b16d6637797928351029
Isnack Souza <isnack_yagami@hotmail.com>
2015-05-30 06:31:20 -0300

    Modificação no banco e commit dos SQL para criação do banco

--- helpsystem/Conexao.php
+++ helpsystem/Conexao.php
@@ -2,15 +2,15 @@
 $host="localhost";
 //Banco Local
 
-//$user = "root";
-//$pass = "clara02";
-//$banco = "local";
+$user = "root";
+$pass = "clara02";
+$banco = "local";
 
 //Banco Online
 
-$user = "root";
-$pass = "clara02";
-$banco = "local2";
+//$user = "root";
+//$pass = "clara02";
+//$banco = "local2";
 
 mysql_connect($host, $user,$pass) or die("BANCO NAO CONECTOU");
 mysql_select_db($banco)or die("nao selecionou banco");
